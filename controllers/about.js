9bbc17a2505aa102aa859fc44369c6299e39ca5f
Glitch (lackadaisical-sour-action) <none>
2022-08-06 19:30:17 +0000

    🎯🐇 Checkpoint ./views/partials/menu.hbs:647556/29 ./views/about.hbs:647556/321 ./controllers/about.js:647556/22 ./controllers/dashboard.js:647556/48 ./models/station-store.js:647556/84

--- controllers/about.js
+++ controllers/about.js
@@ -6,7 +6,7 @@ const about = {
   index(request, response) {
     logger.info("about rendering");
     const viewData = {
-      title: "About Template 1",
+      title: "About WeatherTopv2",
     };
     response.render("about", viewData);
   },
---
+++ models/station-store.js
@@ -0,0 +1,5 @@
+"use strict";
+const logger = require("../utils/logger");
+
+
+module.exports = station;
