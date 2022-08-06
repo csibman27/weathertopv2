64839d9477103ccaa32106fc39b551e029f027da
Glitch (lackadaisical-sour-action) <none>
2022-08-06 19:41:18 +0000

    👹🎇 Checkpoint ./models/station-store.js:647556/776 ./controllers/dashboard.js:647556/142

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,13 +1,16 @@
 "use strict";
 
 const logger = require("../utils/logger");
+const sonatas = require('../models/station-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "WeatherTopV2 Dashboard",
+      station: stations,
     };
+    logger.info('about to render', station)
     response.render("dashboard", viewData);
   },
 };
--- models/station-store.js
+++ models/station-store.js
@@ -1,5 +1,23 @@
 "use strict";
 const logger = require("../utils/logger");
 
+const station = {
+  title: 'Beethoven Sonatas',
+  songs: [
+    {
+      title: 'Piano Sonata No. 3',
+      artist: 'Beethoven',
+    },
+    {
+      title: 'Piano Sonata No. 7',
+      artist: 'Beethoven',
+    },
+    {
+      title: 'Piano Sonata No. 10',
+      artist: 'Beethoven',
+    },
+  ],
+};
+    
 
 module.exports = station;
