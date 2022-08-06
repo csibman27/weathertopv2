3a1b0ec89eadfaf294b692da7b3d795bb19b123e
Glitch (lackadaisical-sour-action) <none>
2022-08-06 20:51:23 +0000

    😽🕶 Checkpoint ./routes.js:647556/6022 ./views/partials/addstation.hbs:647556/689 ./views/partials/addreading.hbs:647556/1281 ./views/partials/listreading.hbs:647556/866 ./views/partials/liststations.hbs:647556/693 ./views/partials/welcomemenu.hbs:647556/319 ./views/dashboard.hbs:647556/6

--- routes.js
+++ routes.js
@@ -11,3 +11,32 @@ router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
 
 module.exports = router;
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const accounts = require('./controllers/accounts.js');
+
+const station = require('./controllers/station.js');
+const dashboard = require("./controllers/dashboard.js");
+const about = require("./controllers/about.js");
+const request = require("./utils/request.js");
+
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
+
+router.get('/station/:id', station.index);
+router.get("/dashboard", dashboard.index);
+router.get("/about", about.index);
+router.get('/station/:id/deletereading/:readingid', station.deleteReading);
+router.get('/dashboard/deletestation/:id', dashboard.deleteStation);
+router.post('/station/:id/addreading', station.addReading);
+router.post('/station/:id/autoreading', station.autoReading);
+router.post('/dashboard/addstation', dashboard.addStation);
+
+
+module.exports = router;
