9308ff07676404dd5b98f9ecc9e6fec8c38abcac
Glitch (lackadaisical-sour-action) <none>
2022-08-07 09:41:11 +0000

    🙉🎉 Checkpoint ./package.json:647556/68 ./routes.js:647556/442 ./models/station-store.json:647556/22 ./models/user-store.json:647556/14

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ const accounts = require('./controllers/accounts.js');
 const station = require('./controllers/station.js');
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
-//const request = require("./utils/request.js");
+
 
 router.get('/', accounts.index);
 router.get('/login', accounts.login);
@@ -20,11 +20,8 @@ router.post('/authenticate', accounts.authenticate);
 router.get('/station/:id', station.index);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
-//router.get('/station/:id/deletereading/:readingid', station.deleteReading);
-//router.get('/dashboard/deletestation/:id', dashboard.deleteStation);
 router.post('/station/:id/addreading', station.addReading);
-//router.post('/station/:id/autoreading', station.autoReading);
-//router.post('/dashboard/addstation', dashboard.addStation);
+
 
 
 module.exports = router;
