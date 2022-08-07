ed589219daa29c64bec01571260f877420f2198c
Glitch (lackadaisical-sour-action) <none>
2022-08-07 09:52:15 +0000

    🌵🌾 Checkpoint ./views/partials/welcomemenu.hbs:647556/123 ./views/partials/menu.hbs:647556/83 ./routes.js:647556/162

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ const station = require('./controllers/station.js');
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 
-
+//Account
 router.get('/', accounts.index);
 router.get('/login', accounts.login);
 router.get('/signup', accounts.signup);
@@ -17,10 +17,14 @@ router.get('/logout', accounts.logout);
 router.post('/register', accounts.register);
 router.post('/authenticate', accounts.authenticate);
 
+//station
 router.get('/station/:id', station.index);
+router.post('/station/:id/addreading', station.addReading);
+
+//menu
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
-router.post('/station/:id/addreading', station.addReading);
+
 
 
 
