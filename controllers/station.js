f2f9cbdd723f2616fdbabf1b9caa6b1c8583aa0c
Glitch (lackadaisical-sour-action) <none>
2022-08-21 12:43:15 +0000

    🏣🏰 Checkpoint ./views/layouts/main.hbs:647556/108 ./views/dashboard.hbs:647556/1190 ./controllers/dashboard.js:647556/1405 ./controllers/station.js:647556/1362

--- controllers/station.js
+++ controllers/station.js
@@ -5,7 +5,6 @@ const stationStore = require("../models/station-store");
 const uuid = require("uuid");
 const currentDate = new Date();
 const stationAnalytics = require("../utils/station-analytics");
-const axios = require("axios");
 
 
 const currentDayOfMonth = currentDate.getDate();
@@ -68,43 +67,6 @@ const station = {
     logger.debug("New Reading = ", newReading);
     
     stationStore.addReading(stationId, newReading);
-    response.redirect("/station/" + stationId);
-  },
-  
-  async autoReading(request, response) {
-    try {
-      const stationId = request.params.id;
-      const station = stationStore.getStation(stationId);
-      let report = {};
-      const lat = station.latitude;
-      const lng = station.longitude;
-      const requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=6636e8a011274b26236f823786718c14`;
-      const result = await axios.get(requestUrl);
-      if (result.status == 200) {
-        const reading = result.data.current;
-
-        report.code = reading.weather[0].id;
-        report.temp = reading.temp;
-        report.windSpeed = reading.wind_speed;
-        report.pressure = reading.pressure;
-        report.windDirection = reading.wind_deg;
-        report.timestamp = Date();
-      }
-      stationStore.addAutoReading(stationId, report);
-      response.redirect("/station/" + stationId);
-      console.log(report);
-    } catch (error) {
-      console.error(error);
-    }
-  },
-
-  deleteAutoReading(request, response) {
-    const stationId = request.params.id;
-    const readingId = request.params.readingid;
-    logger.debug(
-      `Deleting Auto Reading ${readingId} from Station ${stationId}`
-    );
-    stationStore.removeReading(stationId, readingId);
     response.redirect("/station/" + stationId);
   }
 };
