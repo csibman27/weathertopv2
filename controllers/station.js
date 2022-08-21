c32a17acd57d6935f8ea400db0ce72706d31811a
Glitch (lackadaisical-sour-action) <none>
2022-08-21 14:38:27 +0000

    🏰🍡 Checkpoint ./controllers/station.js:647556/793 ./views/partials/addreading.hbs:647556/7

--- controllers/station.js
+++ controllers/station.js
@@ -72,30 +72,42 @@ const station = {
   },
   
   async autoReading(request, response) {
-    try {
+    logger.info("rendering new report");
+    let report = {};
       const stationId = request.params.id;
       const station = stationStore.getStation(stationId);
-      let report = {};
       const lat = station.latitude;
       const lng = station.longitude;
       const requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&units=metric&appid=6636e8a011274b26236f823786718c14`;
       const result = await axios.get(requestUrl);
       if (result.status == 200) {
         const reading = result.data.current;
-
         report.code = reading.weather[0].id;
         report.temp = reading.temp;
         report.windSpeed = reading.wind_speed;
         report.pressure = reading.pressure;
         report.windDirection = reading.wind_deg;
         report.timestamp = Date();
+        report.tempTrend = [];
+        report.trendLabels = [];
+        const trends = result.data.daily;
+        for (let i=0; i<trends.length; i++) {
+          report.tempTrend.push(trends[i].temp.day);
+          const date = new Date(trends[i].dt * 1000);
+          console.log(date);
+          report.trendLabels.push(`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}` );
+        }
       }
-      stationStore.addAutoReading(stationId, report);
-      response.redirect("/station/" + stationId);
-      console.log(report);
-    } catch (error) {
-      console.error(error);
-    }
+  
+      const viewData = {
+        title: "Weather Report",
+        reading: report
+    };
+    response.render("dashboard", viewData);
+    stationStore.addAutoReading(stationId, report);
+    response.redirect("/station/" + stationId);
+    console.log(report);
+
   }
 };
 
